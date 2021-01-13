6570ad5948f5d0da44868739942804f63220c382
stmcf <stevenmcfarlane@hotmail.co.uk>
2021-01-13 21:07:06 +0000

    CSS styling changed to have a split screen view port. added CSS and alignment. Looks better now. Further work to do once wire-framing and portfolio projects are complete.  OK to merge to main now.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
       <h3>Steven McFarlane;</h3>
         <h4>Software Developer.</h4>
             <p>
@@ -16,7 +16,29 @@ function App() {
             <li><a href="https://github.com/stmcf" target="_blank" rel="noreferrer">GitHub</a></li>
             <li><a href="https://twitter.com/IAMSMCF" target="_blank" rel="noreferrer">Twitter</a></li>
           </ul>
-      </header>
+      </header> */}
+      <div class="split left">
+        <div class="centered">
+          {/* <h5>Hi ! I'm </h5> */}
+          <h3>Steven McFarlane;</h3>
+          <h4>Software Developer</h4>
+        </div>
+      </div>
+
+      <div class="split right">
+        <div class="centered">
+          
+            <p>
+              I'm working on my new site, until then you can contact me on the following platforms: 
+            </p>
+            <ul class="nav-links right">
+            <li><a href="mailto: stevenmcfarlane@hotmail.co.uk">Email</a></li>
+            <li><a href="https://www.linkedin.com/in/steven-mcfarlane-9b769637" target="_blank" rel="noreferrer">LinkedIn</a></li>
+            <li><a href="https://github.com/stmcf" target="_blank" rel="noreferrer">GitHub</a></li>
+            <li><a href="https://twitter.com/IAMSMCF" target="_blank" rel="noreferrer">Twitter</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
