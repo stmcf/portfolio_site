628f94985cb323bb5ae388c2766be3fef5a70fa2
stmcf <stevenmcfarlane@hotmail.co.uk>
2021-01-13 10:43:43 +0000

    react web-app created and holding page styled. favicon amended, links to social media etc all added and adobe font installed for use going-forward.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h3>Steven McFarlane;</h3>
+        <h4>Software Developer.</h4>
+            <p>
+              I'm working on my new site, until then you can contact me on the following platforms: 
+            </p>
+          <ul>
+            <li><a href="mailto: stevenmcfarlane@hotmail.co.uk">Email</a></li>
+            <li><a href="https://www.linkedin.com/in/steven-mcfarlane-9b769637" target="_blank" rel="noopener">LinkedIn</a></li>
+            <li><a href="https://github.com/stmcf" target="_blank" rel="noopener">GitHub</a></li>
+            <li><a href="https://twitter.com/IAMSMCF" target="_blank" rel="noopener">Twitter</a></li>
+          </ul>
       </header>
     </div>
   );
